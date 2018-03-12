bfd64857fc6c6071b0c66cba908da3bbcf3dd820
Niklas Jørgensen <niklas220587@hotmail.com>
2018-03-12 12:37:58 +0100

    added testing db for developer

---
+++ Centisoft.Core/DAL/TestingDeveloperRepo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Centisoft.Core.Contracts;
+using Centisoft.Core.Models;
+
+namespace Centisoft.Core.DAL
+{
+    public class TestingDeveloperRepo : IGenericInterface<Developer>
+    {
+        public List<Developer> TheDeveloperList { get; set; }
+        public void Delete(Developer t)
+        {
+            TheDeveloperList.Remove(t);
+        }
+
+        public Developer Get(int id)
+        {
+            return TheDeveloperList.Find(x => x.Id == id);            
+        }
+
+        public List<Developer> GetAll()
+        {
+            return TheDeveloperList;
+        }
+
+        public void Save(Developer t)
+        {
+            TheDeveloperList.Add(t);
+        }
+    }
+}
